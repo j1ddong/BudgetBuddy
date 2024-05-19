97b867f99fd654b8a013c7902ea7cd100daf54d5
j1ddong <hju191228@gmail.com>
2024-05-18 23:31:14 -0700

    implement confirm email

---
+++ buget-buddy/src/app/accounts/confirm-email/page.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+import { Button } from '@mantine/core';
+import authStyles from '@/app/accounts/auth.module.css';
+
+const ConfirmEmail = ({ searchParams }: confirmUrlSearchParamsType) => {
+	return (
+		<>
+			<p className={authStyles.authInfo}>
+				<b>Please activate your account!</b>
+			</p>
+			<Button
+				className={authStyles.confirmationBtn}
+				fullWidth
+				component={Link}
+				href={searchParams.confirmUrl}
+			>
+				Confirm the email
+			</Button>
+		</>
+	);
+};
+
+export default ConfirmEmail;
