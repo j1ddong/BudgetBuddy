dbef639a46fe6e80e3af96acfe7fb5a2ffa10b75
j1ddong <hju191228@gmail.com>
2024-05-22 00:12:26 -0700

    used userGlobalContext

--- buget-buddy/src/app/layout.tsx
+++ buget-buddy/src/app/layout.tsx
@@ -3,6 +3,9 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { ColorSchemeScript, MantineProvider } from '@mantine/core';
+import { AccountDataContextProvider } from '@/contexts/accountContext/accountContext.provider';
+import { createClient } from '@/app/utils/supabase/server';
+import { UserDataContextProvider } from '@/contexts/userContext/userContext.provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -11,18 +14,27 @@ export const metadata: Metadata = {
 	description: 'Manage your money with BudgetBuddy',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
+	const supabase = createClient();
+	const {
+		data: { user },
+	} = await supabase.auth.getUser();
+
 	return (
 		<html lang='en'>
 			<head>
 				<ColorSchemeScript />
 			</head>
 			<body className={inter.className}>
-				<MantineProvider>{children}</MantineProvider>
+				<MantineProvider>
+					<UserDataContextProvider initialData={{ user }}>
+						{children}
+					</UserDataContextProvider>
+				</MantineProvider>
 			</body>
 		</html>
 	);
